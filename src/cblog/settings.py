3ab92c3912455357e84eefe720d8912546dc8cb0
abdullahakyuz <abdullah.akyuz90@gmail.com>
2023-07-22 14:47:23 +0300

    reziyon 2

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clarusway', # database name in RDS is written here
+        'NAME': 'aws-capstone-rds', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'clarusway.cckaqahn4jix.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'aws-capstone-rds.cckaqahn4jix.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
